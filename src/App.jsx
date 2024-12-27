e5c07c9146716b86eec80eab01db2e6ea6853618
Jhonnatan Espinoza Rojas <jhonnatan.espinoza.r@uni.pe>
2024-12-27 09:50:08 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import ScratchCard from './components/ScratchCard';
 function App() {
 	return (
 		<div className="flex justify-center items-center select-none min-h-screen bg-gradient-to-br from-rose-200 to-fuchsia-200">
-			<div className="flex items-center justify-center bg-pattern-snowflake bg-pattern-small w-full h-full">
-                <div className='p-4'>
+			<div className="flex items-center justify-center bg-pattern-snowflake bg-pattern-small w-full min-h-screen">
+                <div className='p-4 flex flex-col items-center'>
                     <ScratchCard />
                 </div>
 			</div>
